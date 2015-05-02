e1ff2ca038f1ca198ff0b588ce54e7d6d6411865
Truthinthelight <Nickz3@cox.net>
2015-05-01 17:29:31 -0700

    Card Dreadnought
    Added the "Dreadnought" card.

--- src/com/heartstone/main/Main.java
+++ src/com/heartstone/main/Main.java
@@ -14,6 +14,11 @@ public class Main {
 		System.out.println("Spartak attacked nick");
 		System.out.printf("Spartak has %d damage and %d health\n", spartak.damage, spartak.health);
 		System.out.printf("Nick has %d damage and %d health\n", nick.damage, nick.health);
+		Card Dreadnought = new Card(6,5);
+		System.out.printf("Dreadnought has %d damage and %d health\n", Dreadnought.damage, Dreadnought.health);
+		Dreadnought.attack(spartak);
+		System.out.printf("Dreadnought attacked spartak\n");
+		System.out.printf("spartak has %d damage and %d health\n", spartak.damage, spartak.health);
 	}
 	void go(){
 		while(true){
