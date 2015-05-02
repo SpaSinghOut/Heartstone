740185c48d0e2469b3a2104644b37fef4e9bd922
Truthinthelight <Nickz3@cox.net>
2015-05-01 17:50:34 -0700

    leaps

--- src/com/heartstone/main/Main.java
+++ src/com/heartstone/main/Main.java
@@ -10,9 +10,9 @@ public class Main {
 		Card nightwolf = new Card(3,3,3);
 		Card burningCrusader = new Card(2,1,3);
 		Card dreadnought = new Card(5,6,5);
-		Card savage = new Card(3,5,1);
+		Card savageLion = new Card(3,5,1);
 		Card bandit = new Card(2,2,2);
-		Card tiger = new Card(3,4,2);
+		Card leapingTiger = new Card(3,4,2);
 		Card wisp = new Card(1,1,1);
 		Card blackAngel = new Card(8,9,7);
 		Card ghoul = new Card(2,2,4);
