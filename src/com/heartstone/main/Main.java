afbf9bbd90da7a8d848c54ecb70cae8855954274
Truthinthelight <Nickz3@cox.net>
2015-05-01 22:33:03 -0700

    Changed some card names and added heroes

--- src/com/heartstone/main/Main.java
+++ src/com/heartstone/main/Main.java
@@ -6,18 +6,22 @@ public class Main {
 		new Main().go();
 	}
 	Main(){
-		Card fierceGnome = new Card(1,2,1);
-		Card nightwolf = new Card(3,3,3);
+		Card lavaGolem = new Card(1,2,1);
+		Card direWolf = new Card(3,3,3);
 		Card burningCrusader = new Card(2,1,3);
-		Card dreadnought = new Card(5,6,5);
-		Card savageLion = new Card(3,5,1);
+		Card juggernaut = new Card(6,2,9);
+		Card mysteriousMan = new Card(3,5,1);
 		Card bandit = new Card(2,2,2);
-		Card leapingTiger = new Card(3,4,2);
+		Card sabreToothTiger = new Card(3,4,2);
 		Card wisp = new Card(1,1,1);
-		Card blackAngel = new Card(8,9,7);
-		Card ghoul = new Card(2,2,4);
+		Card darkAngel = new Card(8,9,7);
+		Card lostSoul = new Card(2,2,4);
+		
 		Hero aletheia = new Hero(30);
-		System.out.println(aletheia);
+		Hero vladimir = new Hero(30);
+		Hero gabriel = new Hero(30);
+		Hero castiel = new Hero(30);
+		
 		
 	}
 	void go(){
